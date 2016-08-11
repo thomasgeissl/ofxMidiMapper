1a557b2ebf41d78ac866bdebb3b179e97dfbe70d
Thomas Geissl <thomas.geissl@ixds.com>
2016-08-11 13:45:05 +0200

    cleaned up

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,31 +1,29 @@
 #include "ofApp.h"
 
-
-
 void ofApp::setup(){
 	ofSetFrameRate(120);
-    _parameters.setName("parameters");
-    _mappableParameters.setName("mappable parameters");
-    _mappableBoolParameter.set("bool parameter", false);
-    _mappableFloatParameter.set("float parameter", 0,0,1);
-    _mappableIntParameter.set("int parameter", 0,0,100);
+	_parameters.setName("parameters");
+	_mappableParameters.setName("mappable parameters");
+	_mappableBoolParameter.set("bool parameter", false);
+	_mappableFloatParameter.set("float parameter", 0, 0, 1);
+	_mappableIntParameter.set("int parameter", 0, 0, 100);
 
-    _mappableParameters.add(_mappableBoolParameter);
-    _mappableParameters.add(_mappableFloatParameter);
-    _mappableParameters.add(_mappableIntParameter);
+	_mappableParameters.add(_mappableBoolParameter);
+	_mappableParameters.add(_mappableFloatParameter);
+	_mappableParameters.add(_mappableIntParameter);
 
-    _midiMapper.getParameters().setName("Mapper parameters");
-    _parameters.add(_midiMapper.getParameters());
-    _parameters.add(_mappableParameters);
+	_midiMapper.getParameters().setName("Mapper parameters");
+	_parameters.add(_midiMapper.getParameters());
+	_parameters.add(_mappableParameters);
 
-    _gui.setup(_parameters);
+	_gui.setup(_parameters);
 
-    _midiMapper.addParameter(&_mappableBoolParameter);
-    _midiMapper.addParameter(&_mappableFloatParameter);
-    _midiMapper.addParameter(&_mappableIntParameter);
+	_midiMapper.addParameter(&_mappableBoolParameter);
+	_midiMapper.addParameter(&_mappableFloatParameter);
+	_midiMapper.addParameter(&_mappableIntParameter);
 
 //    connect to a midi device
-_midiMapper.openMidiPort(0);
+	_midiMapper.openMidiPort(0);
 //    _midiMapper.openVirtualMidiPort("ofxMidiMapperExample");
 }
 
@@ -39,7 +37,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    _gui.draw();
+	_gui.draw();
 }
 
 //--------------------------------------------------------------
@@ -49,16 +47,20 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-    switch(key)
-    {
-    case 'l': _midiMapper.loadMapping("mapping.json"); break;
-    case 's': _midiMapper.saveMapping("mapping.json"); break;
-    }
+	switch(key){
+	 case 'l':
+		 _midiMapper.loadMapping("mapping.json");
+		 break;
+
+	 case 's':
+		 _midiMapper.saveMapping("mapping.json");
+		 break;
+	}
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y){
 
 }
 
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -4,31 +4,30 @@
 #include "ofxGui.h"
 #include "ofxMidiMapper.h"
 
-class ofApp : public ofBaseApp{
-public:
-	void setup();
-	void update();
-	void draw();
-	void exit();
+class ofApp : public ofBaseApp {
+	public:
+		void setup();
+		void update();
+		void draw();
+		void exit();
 
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-private:
-    ofParameterGroup _parameters;
-    ofParameterGroup _mappableParameters;
-    ofParameter<bool> _mappableBoolParameter;
-    ofParameter<float> _mappableFloatParameter;
-    ofParameter<int> _mappableIntParameter;
-    ofxPanel _gui;
-    ofxMidiMapper _midiMapper;
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+	private:
+		ofParameterGroup _parameters;
+		ofParameterGroup _mappableParameters;
+		ofParameter <bool> _mappableBoolParameter;
+		ofParameter <float> _mappableFloatParameter;
+		ofParameter <int> _mappableIntParameter;
+		ofxPanel _gui;
+		ofxMidiMapper _midiMapper;
 };
-
